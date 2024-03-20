d9f130a47e84d85ba5a3e0941f1d8dc4bc9dea55
Mariana <mariana.ssousasilva@gmail.com>
2024-03-20 14:32:04 +0100

    Added API, AJAX and JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateHaiku(event) {
-  event.preventDefault();
-
+function displayHaiku(response) {
   new Typewriter("#haiku", {
-    strings: "The earth shakes, just enough, to remind us.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateHaiku(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "00a3d0oe3b36e11ff9bf2a3fd4b8t806";
+  let context =
+    "You are a haiku expert. Your mission is to generate a haiku with 3 phrases in basic html. Make sure to follow the user instructions";
+  let prompt = `User instructions: Generate a haiku about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayHaiku);
 }
 
 let haikuFormElement = document.querySelector("#haiku-generator-form");
